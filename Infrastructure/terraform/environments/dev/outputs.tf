1fcbbd8cfc60f9d3db4a1598ba1818ddb767c991
Zamshed87 <zamshedforman130442@gmail.com>
2025-08-31 11:42:45 +0000

    Updates the ec2 key pair section

--- Infrastructure/terraform/environments/dev/outputs.tf
+++ Infrastructure/terraform/environments/dev/outputs.tf
@@ -1,3 +1,4 @@
+# VPC outputs
 output "vpc_id" {
   value = module.vpc.vpc_id
 }
@@ -6,10 +7,16 @@ output "public_subnet_id" {
   value = module.vpc.public_subnet_id
 }
 
+# EC2 outputs
 output "ec2_public_ip" {
   value = module.ec2.public_ip
 }
 
+output "ec2_private_key_path" {
+  value = module.ec2.private_key_path
+}
+
+# ECR output
 output "ecr_repository_url" {
   value = module.ecr.repository_url
 }
